8a6792059b0b88d2cab6b67dd19104124868d823
akaunderr <akaunderr@gmail.com>
2024-05-22 14:09:32 +0300

    updated module wrapper

--- ocmodw.ini.php
+++ ocmodw.ini.php
@@ -6,21 +6,9 @@ define('MODYEAR', '2019');
 define('AUTHOR', 'Andrii Burkatskyi');
 define('EMAIL', 'ocmod.space@gmail.com');
 
-define('MODLINK', 'https://github.com/ocmod-space/ocmod-oc-fixes');
+define('MODLINK', 'https://github.com/ocmod-space/ocmod-aux');
 define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
-define('GITLINK', 'https://github.com/ocmod-space/ocmod-oc-fixes');
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-aux');
 define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
 
-define('LICLINK', 'https://github.com/ocmod-space/ocmod-oc-fixes/blob/main/LICENSE.txt');
-
-///
-
-define('FCLDIR', '_fcl');
-define('FCLIGNORE', '.fclignore'); // ignorelist for fcl
-define('SRCDIR', 'src');
-define('ZIPDIR', 'zip');
-define('ZIPEXT', '.ocmod.zip');
-
-define('MDIR', 'module'); // dir with module
-define('ADIR', 'addons'); // dir with addons
-
+define('LICLINK', 'https://github.com/ocmod-space/ocmod-aux/blob/main/LICENSE.txt');
